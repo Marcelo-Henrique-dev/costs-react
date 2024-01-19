7c1a9285cfa87f48c52dfc97afe8f3803f356ebd
Marcelo-Henrique_dev <dvmarcelohenrique@gmail.com>
2024-01-19 10:58:03 -0300

    estrutura da pagina de novo projeto

--- src/components/pages/NovoProjeto.js
+++ src/components/pages/NovoProjeto.js
@@ -1,6 +1,14 @@
+import ProjectForm from '../projects/ProjectForm'
+import styles from './NovoProjeto.module.css'
+
 function NovoProjeto(){
     return(
-        <h1>Novo Projeto</h1>
+        <div className={styles.novoProjeto_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <p>Formulário</p>
+            <ProjectForm></ProjectForm>
+        </div>
     )
 }
 
