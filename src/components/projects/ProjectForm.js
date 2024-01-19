85e43a700ab545792fd034bd4652e28aa8ca2910
Marcelo-Henrique_dev <dvmarcelohenrique@gmail.com>
2024-01-19 13:08:28 -0300

    criado banco de dados usando json api

--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -2,8 +2,26 @@ import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 import styles from "./ProjectForm.module.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
-function ProjectForm({btnText}) {
+function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/categories", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setCategories(data);
+    })
+    .catch((err) => console.log(err));
+  }, [])
+
   return (
     <form className={styles.form}>
       <Input
@@ -18,8 +36,12 @@ function ProjectForm({btnText}) {
         name="budget"
         placeholder="Insira o orçamento total do projeto"
       ></Input>
-      <Select name="category_id" text="Selecione a categoria"/>
-      <SubmitButton text={btnText}/>
+      <Select
+        name="category_id"
+        text="Selecione a categoria"
+        options={categories}
+      />
+      <SubmitButton text={btnText} />
     </form>
   );
 }
