bfe2856bc3bb830cdb8d7a202f2b5fdd571a570d
Marcelo-Henrique_dev <dvmarcelohenrique@gmail.com>
2024-01-22 13:45:38 -0300

    adicionado loader

--- src/components/pages/Projetos.js
+++ src/components/pages/Projetos.js
@@ -5,9 +5,11 @@ import Container from "../layout/Container";
 import LinkButton from "../layout/LinkButton";
 import Card from "../projects/Card";
 import { useState, useEffect } from "react";
+import Loading from "../layout/Loading";
 
 function Projetos() {
   const [projects, setProjects] = useState([]);
+  const [removeLoading, setRemoveLoading] = useState(false)
 
   const location = useLocation();
   let message = "";
@@ -16,18 +18,21 @@ function Projetos() {
   }
 
   useEffect(() => {
-    fetch("http://localhost:5000/projects", {
-      method: "GET",
-      headers: {
-        "content-type": "application/json",
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log(data);
-        setProjects(data);
+    setTimeout(() => {
+      fetch("http://localhost:5000/projects", {
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+        },
       })
-      .catch((err) => console.log(err));
+        .then((resp) => resp.json())
+        .then((data) => {
+          console.log(data);
+          setProjects(data);
+          setRemoveLoading(true);
+        })
+        .catch((err) => console.log(err));
+    }, 300);
   }, []);
 
   return (
@@ -48,6 +53,10 @@ function Projetos() {
               key={project.id}
             />
           ))}
+          {!removeLoading && <Loading />}
+          {removeLoading && projects.length === 0 && (
+            <p>Não Há Projetos Cadastrados</p>
+          )}
       </Container>
     </div>
   );
